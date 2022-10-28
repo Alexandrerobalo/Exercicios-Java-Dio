6660c465bd20d14f300703573d31fc5d365d9c77
Alexandre <xandirsilva64@gmail.com>
2022-10-28 12:07:37 -0300

    Exercício Par ou Impar

--- src/Ex4_ParImpar.java
+++ src/Ex4_ParImpar.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ex4_ParImpar {
    /*Faça um programa que peça N números inteiros,
    calcule e mostre a quantidade de números pares
@@ -6,6 +8,31 @@ public class Ex4_ParImpar {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+
+        int quantNumeros;
+        int contador= 0;
+        int numero;
+        int quantPar = 0;
+        int quantImpar = 0;
+
+        System.out.println("Quantidade de números: ");
+        quantNumeros = scan.nextInt();
+
+        do {
+
+            System.out.println("Numero:");
+            numero = scan.nextInt();
+
+            if (numero % 2 == 0 ) quantPar++ ;
+            else quantImpar++;
+
+
+            contador = contador + 1;
+        } while (contador < quantNumeros );
+
+        System.out.println("Quantidade Par:" + quantPar);
+        System.out.println("Quantidade impar"+ quantImpar);
 
 
 
