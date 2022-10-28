20b3d193ee0662591bf4c94482213ad7bc246227
Alexandre <xandirsilva64@gmail.com>
2022-10-28 09:46:05 -0300

    alteração

---
+++ src/Ex2_Nota.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Ex2_Nota {
+/*Faça um programa que peça uma nota, entre zero e dez.
+    Mostre uma mensagem caso o valor seja invalido
+    e continue pedindo
+    até que o usuário informe um valor válido.
+ */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("nota:");
+         nota = scan.nextInt();
+
+         while (nota < 0  | nota > 10){
+             System.out.println("Nota inválida! Digite novamente");
+            nota = scan.nextInt();
+
+        }
+
+
+
+    }
+
+
+
+}
