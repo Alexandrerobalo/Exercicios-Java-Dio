689dbba7548b4882ee3beb069a800e43e5962f46
Alexandre <xandirsilva64@gmail.com>
2022-10-28 11:31:38 -0300

    Exercício Par ou Impar

---
+++ src/Ex4_ParImpar.java
@@ -0,0 +1,18 @@
+public class Ex4_ParImpar {
+   /*Faça um programa que peça N números inteiros,
+   calcule e mostre a quantidade de números pares
+   e a quantidade de números impares.
+    */
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+    }
+
+
+}
