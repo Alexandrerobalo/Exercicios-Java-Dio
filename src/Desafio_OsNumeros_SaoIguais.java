af3ac9910fc46b3f96d456112f3f94c924f0c4f9
Alexandre <xandirsilva64@gmail.com>
2022-11-03 10:52:29 -0300

    Exercício

--- src/Desafio_OsNumeros_SaoIguais.java
+++ src/Desafio_OsNumeros_SaoIguais.java
@@ -5,20 +5,22 @@ public class Desafio_OsNumeros_SaoIguais {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        {
+            int A = 15;
+            int B = 15;
+            System.out.println("Digite a primeira nota:");
+            A = scan.nextInt();
 
-        int A;
-        int B;
-        System.out.println("Digite a primeira nota:");
-        A = scan.nextInt();
-
-         System.out.println("Digite a segunda nota:");
-        B = scan.nextInt();
-
-        while (A  = B  ||  ){
-            System.out.println("Nota inválida! Digite novamente");
+            System.out.println("Digite a segunda nota:");
             B = scan.nextInt();
 
+            if ((A - B) == 0)
 
+            System.out.println("Os números são iguais");
+            else
+            System.out.println("Os números são diferentes");
+
+        }
 
 
 
